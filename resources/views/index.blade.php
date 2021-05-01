6c47aaf2718db6370946944ba96f5da21746bf68
dimasic25 <78664325+dimasic25@users.noreply.github.com>
2021-05-01 11:02:23 +0300

    add seed and factory for News

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,30 +5,20 @@
         <thead>
         <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Title</th>
+            <th scope="col">Publish date</th>
+            <th scope="col">Content</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-        </tr>
+        @foreach($news as $record)
+            <tr>
+                <th scope="row">{{$news->id}}</th>
+                <td>{{$news->title}}</td>
+                <td>{{$news->publish_date}}</td>
+                <td>{{$news->content}}</td>
+            </tr>
+
         </tbody>
     </table>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Шаблон Bootstrap</title>
+    <title>Новости</title>
 
 
     <!-- Bootstrap core CSS -->
